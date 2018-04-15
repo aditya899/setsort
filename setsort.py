6fd20b7654b1f487c48e3a9351119b413397737f
aditya899 <raja.adityaraj899@gmail.com>
2018-04-15 22:09:12 +0530

    Create setsort.py

---
+++ setsort.py
@@ -0,0 +1,20 @@
+def printRepeating(arr, size):
+	
+
+	
+	for i in range(0, size):
+		
+		if arr[abs(arr[i])] >= 0:
+			arr[abs(arr[i])] = -arr[abs(arr[i])]
+		else:
+			print (abs(arr[i]), end = " ")
+			
+n=int(input())
+arr = list(map(int,input().split()))
+arr.sort()
+
+
+printRepeating(arr, n)
+
+
+
